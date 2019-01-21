535ddec4ff761d2f8a02bb1671ac92f4dc1c3802
Kemedi Moara-Nkwe <kemedi@Kemedis-MacBook-Pro.local>
2019-01-21 18:27:31 +0000

    Added Json Support to ODLRestTest

--- src/test/java/com/odl/rest/test/ODLRestTest.java
+++ src/test/java/com/odl/rest/test/ODLRestTest.java
@@ -7,14 +7,26 @@ package com.odl.rest.test;
   In order to access any prometheus based stuff and run them prometheus also has to be running
  */
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.odl.rest.XmlFormatter;
 import org.junit.Assert;
 import org.junit.Test;
+import org.w3c.dom.Document;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import org.json.JSONObject;
 
 public class ODLRestTest
 {
@@ -27,6 +39,7 @@ public class ODLRestTest
         {
             String loginPassword = "admin" + ":" + "admin";
             String encoded = new sun.misc.BASE64Encoder().encode (loginPassword.getBytes());
+            String responseType = "json";
 
             //Get Topology from ODL REST interface
             URL getUrl = new URL("http://localhost:8181/restconf/operational/network-topology:network-topology/");
@@ -34,17 +47,37 @@ public class ODLRestTest
             connection.setDoOutput(true);
             connection.setInstanceFollowRedirects(false);
             connection.setRequestMethod("GET");
-            connection.setRequestProperty("Content-Type", "application/xml");
+            connection.setRequestProperty("Content-Type", "application/" + responseType);
             connection.setRequestProperty("Authorization", "Basic " + encoded);
+
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-            String line = reader.readLine();
-            while (line != null)
-            {
-                System.out.println(line);
-                line = reader.readLine();
-            }
+           if(responseType.equals("json"))
+           {
+               JSONObject json = new JSONObject(readAll(reader));
+
+               //System.out.println(json.toString());
+
+               Gson gson = new GsonBuilder().setPrettyPrinting().create();
+               JsonParser jp = new JsonParser();
+               JsonElement je = jp.parse(json.toString());
+               String prettyJsonString = gson.toJson(je);
+
+               System.out.println(prettyJsonString);
+           }
+           else
+           {
+               String line = reader.readLine();
+               if (line != null)
+               {
+                    System.out.println(line);
+                    System.out.println();
+               }
+           }
+
+
+
             Assert.assertEquals(HttpURLConnection.HTTP_OK, connection.getResponseCode());
             connection.disconnect();
         }
@@ -54,5 +87,14 @@ public class ODLRestTest
         }
 
 
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
     }
 }
